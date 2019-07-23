a76739cef17cf55a1fa61a9b64e18023ff0ee501
gccunanan <gccunanan@gmail.com>
2019-07-23 15:07:21 -0700

    fixed create method in orm.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,4 +16,11 @@ router.put('/burgers/update',function(req,res){
     });
 });
 
+router.post('/burgers/create',function(req,res){
+    burger.create(req.body.burger_name,funtion(result){
+        console.log(burger_name);
+        res.redirect('/');
+    })
+})
+
 module.exports = router;
