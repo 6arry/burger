1fb719e8d1ef04384d04e27c56102dd6e8d9f0e7
gccunanan <gccunanan@gmail.com>
2019-07-23 14:32:13 -0700

    updated index

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const burger = require('../models/burger.js')
 
 router.get('/',function(req,res){
-    res.render('index');
+    burger.all(function(burgers_data){
+        console.log(burger_data);
+        res.render('index',{burger_data});
+    })
 })
 
 module.exports = router;
