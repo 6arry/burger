1ddd4cbfc92f638c2274b8ac4fac4f4c340ffc42
gccunanan <gccunanan@gmail.com>
2019-07-23 15:08:51 -0700

    added router.post to burgers_controller.js to add a burger to our database

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,10 +17,10 @@ router.put('/burgers/update',function(req,res){
 });
 
 router.post('/burgers/create',function(req,res){
-    burger.create(req.body.burger_name,funtion(result){
+    burger.create(req.body.burger_name,function(result){
         console.log(burger_name);
         res.redirect('/');
-    })
-})
+    });
+});
 
 module.exports = router;
